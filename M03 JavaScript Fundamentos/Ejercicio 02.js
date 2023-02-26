5e97effe7a5701212fb8608a726e0e092ccb80bb
liliesofcat <anabel.lassom@gmail.com>
2023-02-25 20:46:04 -0500

    Checkpoint commit Anabel Lasso

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,33 +4,50 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
-}
+   return string;
+   }
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+
+   var suma2 = x + y;
+   return suma2;
+
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var resta2 = x / y;
+   return resta2;
+
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var div2 = x / y;
+   return div2;
+
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var multi2 = x * y;
+   return multi2;
+
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var resto2 = x % y;
+   return resto2;
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
